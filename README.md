1a5fa88bfc48ed7065812709ae6d98d15a32f9d9
vonssy <kak.reyvaldizakaria@gmail.com>
2024-12-23 12:00:51 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Teneo Community Node BOT
+Teneo Community Node BOT
+
+Download Extension Here : [Teneo Community Node](https://chromewebstore.google.com/detail/teneo-community-node/emcclcoaglgcpoognfiggmhnhgabppkm) | Use Code : X8sT2
+
+## Fitur
+
+  - Auto Get Account Information
+  - Auto Run With Auto Proxy if u Choose 1
+  - Auto Use [Monosans Proxy](https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt) if u Choose Run With Auto Proxy
+  - Auto Run With Manual Proxy if u Choose 2
+  - Auto Run Without Proxy if u Choose 3
+  - Auto Connect Node
+  - Auto Send Ping
+  - Multi Account With Thread
+
+## Prasyarat
+
+Pastikan Anda telah menginstal Python3.9 dan PIP.
+
+## Instalasi
+
+1. **Kloning repositori:**
+   ```bash
+   git clone https://github.com/vonssy/Teneo-BOT.git
+   ```
+   ```bash
+   cd Teneo-BOT
+   ```
+
+2. **Instal Requirements:**
+   ```bash
+   pip install -r requirements.txt #or pip3 install -r requirements.txt
+   ```
+
+## Konfigurasi
+
+- **accounts.json:** Anda akan menemukan file `accounts.json` di dalam direktori proyek. Pastikan `accounts.json` berisi data yang sesuai dengan format yang diharapkan oleh skrip. Berikut adalah contoh format file:
+
+  ```bash
+    [
+        {
+            "Email": "your_email_address 1",
+            "Password": "your_password 1"
+        },
+        {
+            "Email": "your_email_address 2",
+            "Password": "your_password 2"
+        }
+    ]
+  ```
+- **manual_proxy.txt:** Anda akan menemukan file `manual_proxy.txt` di dalam direktori proyek. Pastikan `manual_proxy.txt` berisi data yang sesuai dengan format yang diharapkan oleh skrip. Berikut adalah contoh format file:
+  ```bash
+    ip:port #http or socks5 - change schemes in line 110
+    http://ip:port
+    socks4://ip:port
+    socks5://ip:port
+    http://ip:port@user:pass #idk its work or not, cuase i don't have authentic proxy
+    socks4://ip:port@user:pass #idk its work or not, cuase i don't have authentic proxy
+    socks5://ip:port@user:pass #idk its work or not, cuase i don't have authentic proxy
+  ```
+
+## Jalankan
+
+```bash
+python bot.py #or python3 bot.py
+```
+
+## Penutup
+
+Terima kasih telah mengunjungi repository ini, jangan lupa untuk memberikan kontribusi berupa follow dan stars.
+Jika Anda memiliki pertanyaan, menemukan masalah, atau memiliki saran untuk perbaikan, jangan ragu untuk menghubungi saya atau membuka *issue* di repositori GitHub ini.
+
+**vonssy**
---
+++ bot.py
@@ -0,0 +1,369 @@
+from aiohttp import (
+    ClientResponseError,
+    ClientSession,
+    ClientTimeout
+)
+from aiohttp_socks import ProxyConnector
+from colorama import *
+from datetime import datetime
+from fake_useragent import FakeUserAgent
+import asyncio, json, os, pytz
+
+wib = pytz.timezone('Asia/Jakarta')
+
+class Teneo:
+    def __init__(self) -> None:
+        self.headers = {
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.9,id;q=0.8",
+            "Apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag",
+            "Origin": "chrome-extension://emcclcoaglgcpoognfiggmhnhgabppkm",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "cross-site",
+            "User-Agent": FakeUserAgent().random,
+            "X-Client-Info": "supabase-js-web/2.45.4",
+            "X-Supabase-Api-Version": "2024-01-01",
+        }
+        self.proxies = []
+        self.proxy_index = 0
+        self.ping_count = 0
+
+    def clear_terminal(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def log(self, message):
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}{message}",
+            flush=True
+        )
+
+    def welcome(self):
+        print(
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Auto Ping {Fore.BLUE + Style.BRIGHT}Teneo - BOT
+            """
+            f"""
+        {Fore.GREEN + Style.BRIGHT}Rey? {Fore.YELLOW + Style.BRIGHT}<INI WATERMARK>
+            """
+        )
+
+    def format_seconds(self, seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02}"
+    
+    async def load_auto_proxies(self):
+        url = "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/all.txt"
+        try:
+            async with ClientSession(timeout=ClientTimeout(total=20)) as session:
+                async with session.get(url=url) as response:
+                    response.raise_for_status()
+                    content = await response.text()
+                    with open('proxy.txt', 'w') as f:
+                        f.write(content)
+
+                    self.proxies = content.splitlines()
+                    if not self.proxies:
+                        self.log(f"{Fore.RED + Style.BRIGHT}No proxies found in the downloaded list!{Style.RESET_ALL}")
+                        return
+                    
+                    self.log(f"{Fore.GREEN + Style.BRIGHT}Proxies successfully downloaded.{Style.RESET_ALL}")
+                    self.log(f"{Fore.YELLOW + Style.BRIGHT}Loaded {len(self.proxies)} proxies.{Style.RESET_ALL}")
+                    self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+                    await asyncio.sleep(3)
+        except Exception as e:
+            self.log(f"{Fore.RED + Style.BRIGHT}Failed to load proxies: {e}{Style.RESET_ALL}")
+            return []
+        
+    async def load_manual_proxy(self):
+        try:
+            if not os.path.exists('manual_proxy.txt'):
+                print(f"{Fore.RED + Style.BRIGHT}Proxy file 'manual_proxy.txt' not found!{Style.RESET_ALL}")
+                return
+
+            with open('manual_proxy.txt', "r") as f:
+                proxies = f.read().splitlines()
+
+            self.proxies = proxies
+            self.log(f"{Fore.YELLOW + Style.BRIGHT}Loaded {len(self.proxies)} proxies.{Style.RESET_ALL}")
+            self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+            await asyncio.sleep(3)
+        except Exception as e:
+            print(f"{Fore.RED + Style.BRIGHT}Failed to load manual proxies: {e}{Style.RESET_ALL}")
+            self.proxies = []
+
+    def get_next_proxy(self):
+        if not self.proxies:
+            self.log(f"{Fore.RED + Style.BRIGHT}No proxies available!{Style.RESET_ALL}")
+            return None
+
+        proxy = self.proxies[self.proxy_index]
+        self.proxy_index = (self.proxy_index + 1) % len(self.proxies)
+        return proxy
+    
+    def check_proxy_schemes(self, proxies):
+        schemes = ["http://", "https://", "socks4://", "socks5://"]
+        if any(proxies.startswith(scheme) for scheme in schemes):
+            return proxies
+        
+        return f"http://{proxies}"
+    
+    def load_accounts(self):
+        try:
+            if not os.path.exists('accounts.json'):
+                self.log(f"{Fore.RED}File 'accounts.json' tidak ditemukan.{Style.RESET_ALL}")
+                return
+
+            with open('accounts.json', 'r') as file:
+                data = json.load(file)
+                if isinstance(data, list):
+                    return data
+                return []
+        except json.JSONDecodeError:
+            return []
+    
+    def hide_email(self, email):
+        local, domain = email.split('@', 1)
+        hide_local = local[:3] + '*' * 3 + local[-3:]
+        return f"{hide_local}@{domain}"
+        
+    async def user_login(self, email: str, password: str, proxy=None):
+        url = "https://node-community-api.teneo.pro/auth/v1/token?grant_type=password"
+        data = json.dumps({"email":email, "password":password, "gotrue_meta_security":{}})
+        headers = {
+            **self.headers,
+            "Content-Length": str(len(data)),
+            "Content-Type": "application/json"
+
+        }
+        connector = ProxyConnector.from_url(proxy) if proxy else None
+        try:
+            async with ClientSession(connector=connector, timeout=ClientTimeout(total=20)) as session:
+                async with session.post(url=url, headers=headers, data=data) as response:
+                    response.raise_for_status()
+                    return await response.json()
+        except (Exception, ClientResponseError) as e:
+            return None
+        
+    async def connect_websocket(self, user_id: str, email: str, proxy=None):
+        wss_url = f"wss://secure.ws.teneo.pro/websocket?userId={user_id}&version=v0.2"
+        try:
+            while True:
+                connector = ProxyConnector.from_url(proxy) if proxy else None
+                session = ClientSession(connector=connector, timeout=ClientTimeout(total=20))
+
+                try:
+                    async with session:
+                        async with session.ws_connect(wss_url) as wss:
+                            self.log(
+                                f"{Fore.MAGENTA + Style.BRIGHT}[ Websocket{Style.RESET_ALL}"
+                                f"{Fore.GREEN + Style.BRIGHT} Is Connected {Style.RESET_ALL}"
+                                f"{Fore.WHITE + Style.BRIGHT}With Proxy {proxy}{Style.RESET_ALL}"
+                                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                            )
+                            while True:
+                                try:
+                                    msg = await wss.receive_json(timeout=10)
+                                    point_today = msg.get("pointsToday", 0)
+                                    point_total = msg.get("pointsTotal", 0)
+                                    self.log(
+                                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} {email} {Style.RESET_ALL}"
+                                        f"{Fore.GREEN + Style.BRIGHT}Received Message{Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT} ] [ Points{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} Today {point_today} {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}-{Style.RESET_ALL}"
+                                        f"{Fore.WHITE + Style.BRIGHT} Total {point_total} {Style.RESET_ALL}"
+                                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                                    )
+
+                                    for _ in range(90):
+                                        await wss.send_json({"type": "PING"})
+                                        self.ping_count += 1
+                                        print(
+                                            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                                            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                                            f"{Fore.BLUE + Style.BRIGHT}Next Message in 15 Minutes.{Style.RESET_ALL}"
+                                            f"{Fore.YELLOW + Style.BRIGHT} Wait... {Style.RESET_ALL}",
+                                            end="\r"
+                                        )
+                                        await asyncio.sleep(10)
+
+                                except asyncio.TimeoutError:
+                                    self.log(f"{Fore.YELLOW + Style.BRIGHT}Timeout, reconnecting...{Style.RESET_ALL}")
+                                    break
+
+                except Exception as e:
+                    print(
+                        f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT}Websocket Isn't Connected.{Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT} Retrying... {Style.RESET_ALL}",
+                        end="\r"
+                    )
+                    if proxy:
+                        proxies = self.get_next_proxy()
+                        proxy = self.check_proxy_schemes(proxies)
+
+        except asyncio.CancelledError:
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Websocket{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} {email} {Style.RESET_ALL}"
+                f"{Fore.GREEN + Style.BRIGHT}Is Closed{Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+            )
+        finally:
+            await session.close()
+        
+    async def question(self):
+        while True:
+            try:
+                print("1. Run With Auto Proxy")
+                print("2. Run With Manual Proxy")
+                print("3. Run Without Proxy")
+                choose = int(input("Choose [1/2/3] -> ").strip())
+
+                if choose in [1, 2, 3]:
+                    proxy_type = (
+                        "With Auto Proxy" if choose == 1 else 
+                        "With Manual Proxy" if choose == 2 else 
+                        "Without Proxy"
+                    )
+                    print(f"{Fore.GREEN + Style.BRIGHT}Run {proxy_type} Selected.{Style.RESET_ALL}")
+                    await asyncio.sleep(1)
+                    return choose
+                else:
+                    print(f"{Fore.RED + Style.BRIGHT}Please enter either 1, 2 or 3.{Style.RESET_ALL}")
+            except ValueError:
+                print(f"{Fore.RED + Style.BRIGHT}Invalid input. Enter a number (1, 2 or 3).{Style.RESET_ALL}")
+        
+    async def process_accounts(self, email: str, password: str, use_proxy: bool):
+        hide_email = self.hide_email(email)
+        proxy = None
+
+        if not use_proxy:
+            login = await self.user_login(email, password)
+            if not login:
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+                    f"{Fore.RED + Style.BRIGHT}Login Failed{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} With Proxy {proxy} {Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                )
+                return
+            
+            user_id = login['user']['id']
+            self.log(
+                f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+                f"{Fore.GREEN + Style.BRIGHT}Login Success{Style.RESET_ALL}"
+                f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+            )
+            await asyncio.sleep(1)
+
+            await self.connect_websocket(user_id, hide_email)
+
+        else:
+            login = None
+            proxies = self.get_next_proxy()
+            proxy = self.check_proxy_schemes(proxies)
+            
+            while login is None:
+                login = await self.user_login(email, password, proxy)
+                if not login:
+                    self.log(
+                        f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+                        f"{Fore.RED + Style.BRIGHT}Login Failed{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} With Proxy {proxy} {Style.RESET_ALL}"
+                        f"{Fore.MAGENTA + Style.BRIGHT}]{Style.RESET_ALL}"
+                    )
+                    await asyncio.sleep(1)
+                
+                    print(
+                        f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+                        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+                        f"{Fore.BLUE + Style.BRIGHT}Try With The Next Proxy,{Style.RESET_ALL}"
+                        f"{Fore.YELLOW + Style.BRIGHT} Wait... {Style.RESET_ALL}",
+                        end="\r",
+                        flush=True
+                    )
+                    
+                    proxies = self.get_next_proxy()
+                    proxy = self.check_proxy_schemes(proxies)
+                    continue
+
+                user_id = login['user']['id']
+                self.log(
+                    f"{Fore.MAGENTA + Style.BRIGHT}[ Account{Style.RESET_ALL}"
+                    f"{Fore.WHITE + Style.BRIGHT} {hide_email} {Style.RESET_ALL}"
+                    f"{Fore.GREEN + Style.BRIGHT}Login Success{Style.RESET_ALL}"
+                    f"{Fore.MAGENTA + Style.BRIGHT} ]{Style.RESET_ALL}"
+                )
+                await asyncio.sleep(1)
+
+                await self.connect_websocket(user_id, hide_email, proxy)
+    
+    async def main(self):
+        try:
+            accounts = self.load_accounts()
+            if not accounts:
+                self.log(f"{Fore.RED}No accounts loaded from 'accounts.json'.{Style.RESET_ALL}")
+                return
+
+            use_proxy_choice = await self.question()
+
+            use_proxy = False
+            if use_proxy_choice in [1, 2]:
+                use_proxy = True
+
+            self.clear_terminal()
+            self.welcome()
+            self.log(
+                f"{Fore.GREEN + Style.BRIGHT}Account's Total: {Style.RESET_ALL}"
+                f"{Fore.WHITE + Style.BRIGHT}{len(accounts)}{Style.RESET_ALL}"
+            )
+            self.log(f"{Fore.CYAN + Style.BRIGHT}-{Style.RESET_ALL}"*75)
+
+            last_proxy_update = None
+            proxy_update_interval = 1800
+
+            if use_proxy and use_proxy_choice == 1:
+                await self.load_auto_proxies()
+                last_proxy_update = datetime.now()
+            elif use_proxy and use_proxy_choice == 2:
+                await self.load_manual_proxy()
+
+            
+            if use_proxy and use_proxy_choice == 1:
+                if not last_proxy_update or (datetime.now() - last_proxy_update).total_seconds() > proxy_update_interval:
+                    await self.load_auto_proxies()
+                    last_proxy_update = datetime.now()
+            
+            tasks = []
+            for account in accounts:
+                email = account.get('Email')
+                password = account.get('Password')
+
+                if email and password:
+                    tasks.append(self.process_accounts(email, password, use_proxy))
+
+            await asyncio.gather(*tasks)
+
+        except Exception as e:
+            self.log(f"{Fore.RED+Style.BRIGHT}Error: {e}{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+    try:
+        bot = Teneo()
+        asyncio.run(bot.main())
+    except KeyboardInterrupt:
+        print(
+            f"{Fore.CYAN + Style.BRIGHT}[ {datetime.now().astimezone(wib).strftime('%x %X %Z')} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.RED + Style.BRIGHT}[ EXIT ] Teneo - BOT{Style.RESET_ALL}                                       "                              
+        )
